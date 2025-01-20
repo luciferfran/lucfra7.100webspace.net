6c3af499839dc0bcc1531422ccb6c915623f1a66
Francisco <luciferfran@gmail.com>
2025-01-20 19:29:46 +0100

    Añadido el despliegue de la dirección IP del visitante en la página de hackeo

--- public/index.php
+++ public/index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HACKED</title>
+    <title>HACKED: <?php echo $_SERVER['REMOTE_ADDR']?></title>
 </head>
 <body>
     <h1>¡¡¡Has sido hackeado!!!</h1>
-		<div>Disculpa las molestias</div>
+    <div>Disculpa las molestias</div>
+    <div>IP: <?php echo $_SERVER['REMOTE_ADDR']?></div>
 </body>
 </html>
